22a5185ee12e20a4a7ca9399a3d98da1ab46f3e5
DevFlip <dev3flip@gmail.com>
2019-07-17 12:58:44 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReCAPTCHA from "react-google-recaptcha";
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const TEST_SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+const TEST_SITE_KEY = "6LcgC64UAAAAAMyi2dXc--rHPvZckl7SSLGRGM7N";
+const DELAY =  500;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props, ...args) {
+    super(props, ...args);
+    this.state = {
+      callback: "not fired",
+      value: "[empty]",
+      load: false,
+      expired: "false"
+    };
+    this._reCaptchaRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ load: true });
+    }, DELAY);
+    console.log("didMount - reCaptcha Ref-", this._reCaptchaRef);
+  }
+
+  handleChange = value => {
+    console.log("Captcha value:", value);
+    this.setState({ value });
+    // if value is null recaptcha expired
+    if (value === null) this.setState({ expired: "true" });
+  };
+
+  asyncScriptOnLoad = () => {
+    this.setState({ callback: "called!" });
+    console.log("scriptLoad - reCaptcha Ref-", this._reCaptchaRef);
+  };
+
+  render() {
+    const { value, callback, load, expired } = this.state || {};
+    return (
+      <div className="App">
+        <h1>
+          <a
+            href="https://github.com/dozoisch/react-google-recaptcha"
+            target="_blank"
+          >
+          <h4 class="me">awew</h4>
+            react-google-recaptcha
+          </a>
+          : 1.0.5
+        </h1>
+        <h2>
+          NOTE: initial load delayed <em>{DELAY / 1000}sec</em> to demonstrate
+          callback
+        </h2>
+        <h3>Recaptcha loaded callback: {callback}</h3>
+        <h5>Recaptcha value: {value}</h5>
+        <h5>Expired: {expired}</h5>
+        {load && (
+          <ReCAPTCHA
+            style={{ display: "inline-block" }}
+            theme="dark"
+            ref={this._reCaptchaRef}
+            sitekey={TEST_SITE_KEY}
+            onChange={this.handleChange}
+            asyncScriptOnLoad={this.asyncScriptOnLoad}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
